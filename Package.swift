2aecb2aa008cf162f6351c920b7ef0cd09f9ff03
Abhishek Saralayaa <abhishek.saralaya@cometchat.com>
2024-03-22 00:52:31 +0530

    4.3.0

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "UIKitSwift",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "UIKitSwift", targets: ["UIKitSwiftWrapper"])
+    ],
+    
+    dependencies: [
+        .package(name: "CometChatSDK",
+            url: "https://github.com/cometchat/chat-sdk-ios.git", .exact("4.0.43")
+        )
+    ],
+    targets: [
+        .target(name: "UIKitSwiftWrapper",
+                dependencies: [
+                    .target(name: "UIKitSwift", condition: .when(platforms: [.iOS])),
+                    .product(name: "CometChatSDK", package: "CometChatSDK")
+                ],
+               path: "UIKitSwiftWrapper"),
+        .binaryTarget(
+            name: "UIKitSwift",
+            url: "https://library.cometchat.io/ios/v4/xcode15/CometChatUIKitSwift_4_3_0_.xcframework.zip",
+            checksum: "b2ace9b0fe30e8b2b2f7cb81e8c2b953fc1fe32a68afd677d442f4b8dd22cafd"
+        )
+    ]
+)
